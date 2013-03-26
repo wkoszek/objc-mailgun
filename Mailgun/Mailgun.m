581c46b378823f7316af36e97d3340cb8c802803
Jay Baird <jay.baird@me.com>
2013-03-26 12:13:51 -0500

    add documentation and updated readme file

--- Mailgun/Mailgun.m
+++ Mailgun/Mailgun.m
@@ -21,12 +21,21 @@ NSString * const kMailgunURL = @"https://api.mailgun.net/v2";
     return client;
 }
 
++ (instancetype)clientWithDomain:(NSString *)domain apiKey:(NSString *)apiKey {
+    NSParameterAssert(domain);
+    NSParameterAssert(apiKey);
+    Mailgun *client = [self client];
+    client.domain = domain;
+    client.apiKey = apiKey;
+    return client;
+}
+
 - (id)initWithBaseURL:(NSURL *)url {
     self = [super initWithBaseURL:url];
     if (self) {
         [self registerHTTPOperationClass:[AFJSONRequestOperation class]];
         [self setDefaultHeader:@"Accept" value:@"application/json"];
-        [self setParameterEncoding:AFJSONParameterEncoding];
+        self.parameterEncoding = AFFormURLParameterEncoding;
     }
     return self;
 }
@@ -35,15 +44,17 @@ NSString * const kMailgunURL = @"https://api.mailgun.net/v2";
     NSParameterAssert(apiKey);
     [self clearAuthorizationHeader];
     [self setAuthorizationHeaderWithUsername:@"api" password:apiKey];
+    _apiKey = apiKey;
 }
 
 - (void)buildFormData:(id<AFMultipartFormData>)formData withAttachments:(NSDictionary *)attachments {
     NSUInteger idx = 1;
-    [attachments enumerateKeysAndObjectsUsingBlock:^(id key, id obj, BOOL *stop) {
-        NSString *type = obj[0];
-        NSData *data = obj[1];
+    [attachments enumerateKeysAndObjectsUsingBlock:^(NSString *filename, NSArray *attachment, BOOL *stop) {
         NSString *name = [NSString stringWithFormat:@"attachment[%d]", idx];
-        [formData appendPartWithFileData:data name:name fileName:key mimeType:type];
+        [formData appendPartWithFileData:attachment[1]
+                                    name:name
+                                fileName:filename
+                                mimeType:attachment[0]];
     }];
 }
 
@@ -68,13 +79,112 @@ NSString * const kMailgunURL = @"https://api.mailgun.net/v2";
 - (void)sendMessage:(MGMessage *)message
             success:(void (^)(NSString *messageId))success
             failure:(void (^)(NSError *error))failure {
+    NSParameterAssert(message);
     AFHTTPRequestOperation *operation = [self HTTPRequestOperationWithRequest:[self createSendRequest:message]
-                                                                      success:^(AFHTTPRequestOperation *operation, id responseObject) {
+                                                                      success:^(AFHTTPRequestOperation *_operation, id responseObject) {
                                                                           if (success) {
                                                                               success(responseObject[@"id"]);
                                                                           }
                                                                       }
-                                                                      failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+                                                                      failure:^(AFHTTPRequestOperation *_operation, NSError *error) {
+                                                                          NSLog(@"%@", error);
+                                                                          if (failure) {
+                                                                              failure(error);
+                                                                          }
+                                                                      }];
+    [self enqueueHTTPRequestOperation:operation];
+}
+
+- (void)sendMessageTo:(NSString *)to
+                 from:(NSString *)from
+              subject:(NSString *)subject
+                 body:(NSString *)body
+              success:(void (^)(NSString *))success
+              failure:(void (^)(NSError *))failure {
+    NSParameterAssert(to);
+    NSParameterAssert(from);
+    NSParameterAssert(subject);
+    MGMessage *message = [MGMessage messageFrom:from to:to subject:subject body:body];
+    [self sendMessage:message success:success failure:failure];
+}
+
+- (void)sendMessageTo:(NSString *)to
+                 from:(NSString *)from
+              subject:(NSString *)subject
+                 body:(NSString *)body {
+    [self sendMessageTo:to from:from subject:subject body:body success:nil failure:nil];
+}
+
+- (void)checkSubscriptionToList:(NSString *)list
+                        email:(NSString *)emailAddress
+                        success:(void (^)(NSDictionary *member))success
+                        failure:(void (^)(NSError *error))failure {
+    NSParameterAssert(list);
+    NSParameterAssert(emailAddress);
+    NSString *messagePath = [NSString stringWithFormat:@"lists/%@/%@/%@", list, @"members", emailAddress];
+    NSURLRequest *request = [self requestWithMethod:@"GET"
+                                               path:messagePath
+                                         parameters:nil];
+    AFHTTPRequestOperation *operation = [self HTTPRequestOperationWithRequest:request
+                                                                      success:^(AFHTTPRequestOperation *_operation, id responseObject) {
+                                                                          if (success) {
+                                                                              success(responseObject);
+                                                                          }
+                                                                      }
+                                                                      failure:^(AFHTTPRequestOperation *_operation, NSError *error) {
+                                                                          NSLog(@"%@", error);
+                                                                          if (failure) {
+                                                                              failure(error);
+                                                                          }
+                                                                      }];
+    [self enqueueHTTPRequestOperation:operation];
+}
+
+- (void)unsubscribeToList:(NSString *)list
+                  email:(NSString *)emailAddress
+                  success:(void (^)())success
+                  failure:(void (^)(NSError *error))failure {
+    NSParameterAssert(list);
+    NSParameterAssert(emailAddress);
+    NSString *messagePath = [NSString stringWithFormat:@"lists/%@/%@/%@", list, @"members", emailAddress];
+    NSURLRequest *request = [self requestWithMethod:@"DELETE"
+                                               path:messagePath
+                                         parameters:nil];
+    AFHTTPRequestOperation *operation = [self HTTPRequestOperationWithRequest:request
+                                                                      success:^(AFHTTPRequestOperation *_operation, id responseObject) {
+                                                                          if (success) {
+                                                                              success();
+                                                                          }
+                                                                      }
+                                                                      failure:^(AFHTTPRequestOperation *_operation, NSError *error) {
+                                                                          NSLog(@"%@", error);
+                                                                          if (failure) {
+                                                                              failure(error);
+                                                                          }
+                                                                      }];
+    [self enqueueHTTPRequestOperation:operation];
+}
+
+- (void)subscribeToList:(NSString *)list 
+                email:(NSString *)emailAddress
+                success:(void (^)())success
+                failure:(void (^)(NSError *error))failure {
+    NSParameterAssert(list);
+    NSParameterAssert(emailAddress);
+    NSString *messagePath = [NSString stringWithFormat:@"lists/%@/%@", list, @"members"];
+    NSDictionary *params = @{@"address": emailAddress,
+                             @"subscribed": @"yes",
+                             @"upsert": @"yes"};
+    NSURLRequest *request = [self requestWithMethod:@"POST"
+                                               path:messagePath
+                                         parameters:params];
+    AFHTTPRequestOperation *operation = [self HTTPRequestOperationWithRequest:request
+                                                                      success:^(AFHTTPRequestOperation *_operation, id responseObject) {
+                                                                          if (success) {
+                                                                              success();
+                                                                          }
+                                                                      }
+                                                                      failure:^(AFHTTPRequestOperation *_operation, NSError *error) {
                                                                           NSLog(@"%@", error);
                                                                           if (failure) {
                                                                               failure(error);
